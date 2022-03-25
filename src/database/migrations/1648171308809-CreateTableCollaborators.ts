c8347f5300e93a08009682e0b4b7eb6bed83af75
Alexandre Abreu <abreuxandi2010@gmail.com>
2022-03-24 22:32:16 -0300

    Migration Collaborators

---
+++ src/database/migrations/1648171308809-CreateTableCollaborators.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateTableCollaborators1648171308809
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "collaborators",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "telNumber",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "cpf",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "password",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("collaborators");
+  }
+}
